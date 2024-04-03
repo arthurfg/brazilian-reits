5799ab572ccefccc913ead7597c86515491fbfe3
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-04-03 00:42:19 -0300

    feat: add trigger-dbt flow & dependencies

---
+++ flows/trigger-dbt/trigger_dbt_flow.py
@@ -0,0 +1,14 @@
+from prefect_dbt.cli.commands import DbtCoreOperation
+from prefect import flow
+
+@flow
+def trigger_dbt_flow() -> str:
+    result = DbtCoreOperation(
+        commands=["dbt run"],
+        project_dir="/Users/apple/prefect-cloud-run-poc/queries",
+        profiles_dir="/Users/apple/prefect-cloud-run-poc/queries"
+    ).run()
+    return result
+
+if __name__ == "__main__":
+    trigger_dbt_flow()
